590dcecb297ebe14bd547e9009f55218c8b85743
Josh H <joshuaharding@trentu.ca>
2021-06-24 02:19:12 -0400

    Finished Match.cs
    no testing done, might need tweaks.

--- JoshuaH_301186067_A2/Match.cs
+++ JoshuaH_301186067_A2/Match.cs
@@ -10,17 +10,41 @@ namespace JoshuaH_301186067_A2
     {
         // Fields:
         // finished : bool
+        private bool finished;
         // teams : Team[]
+        private Team[] teams;
         // winningTeam : Team
+        private Team winningTeam;
 
         // Properties:
         // Finished { get; } : bool
+        public bool Finished { get { return finished; } }
         // MatchNumber { get; set; } : int
+        public int MatchNumber { get; set; }
         // Teams { get; } : Team[]
+        public Team[] Teams { get { return teams; } }
         // WinningTeam { get; set; } : Team
+        public Team WinningTeam { get { return winningTeam; } set { winningTeam = value; } }
 
-        // Methods:
+        // Constructor:
         // Match(int number, Team team1, Team team2)
+        public Match(int number, Team team1, Team team2)
+        {
+            MatchNumber = number;
+            teams = new Team[2];
+            teams[0] = team1;
+            teams[1] = team2;
+            finished = false;
+            winningTeam = null;
+        }
+
         // ToString() : string
+        public override string ToString()
+        {
+            string finishStr;
+            if (finished) { finishStr = "finished"; }
+            else { finishStr = "not finished"; }
+            return $"{teams[0].Name}:{teams[1].Name} {finishStr}, Winner: {winningTeam}";
+        }
     }
 }
